b4b902a47ca930b872d40877d89f3e048d069e9b
Thomas Aleixo Barbosa <thomas.aleixo@ifood.com.br>
2025-05-05 16:36:35 -0300

    commndo de commit para commitar todos os arquivos

---
+++ 1_primeiro_repo/pasta_teste/sub_pasta_teste/arquivo_novo.py
@@ -0,0 +1,4 @@
+print ("hello world 2")
+#ao adicionar uma nova subpasta, é necessário adicionar todos os diretórios anteriores também
+#exemplo, arquivo_novo.py, é necessário adicionar também no comando 1_primeiro_repo/pasta_teste/sub_pasta_teste/arquivo_novo.py
+#no caso sendo <primeiro_diretório>/<sub_diretório_1>/<sub_diretório_subsequente caso tenha>
